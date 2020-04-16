6df575c092de06be7870eeab938507689749f587
printf100 <mickey0502@naver.com>
2020-04-16 20:18:01 +0900

    20200416 Thymeleaf

---
+++ StartBoot03/src/main/java/com/boot/leaf/dto/LeafDto.java
@@ -0,0 +1,37 @@
+package com.boot.leaf.dto;
+
+public class LeafDto {
+
+	private String subject;
+	private int seq;
+
+	public LeafDto() {
+	}
+
+	public LeafDto(String subject, int seq) {
+		this.subject = subject;
+		this.seq = seq;
+	}
+
+	public String getSubject() {
+		return subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public int getSeq() {
+		return seq;
+	}
+
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+
+	@Override
+	public String toString() {
+		return "LeafDto [subject=" + subject + ", seq=" + seq + "]";
+	}
+
+}
